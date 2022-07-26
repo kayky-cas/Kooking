7fbfc28f260b3b1e5bfc52f62e60ee24c2629af9
Kayky Casagrande <kayky-cas@github.com>
2022-07-26 19:34:32 -0300

    Initial Commit

---
+++ Kooking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kooking
+//
+//  Created by Kayky Belleboni Casagrande on 26/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kooking/KookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KookingApp.swift
+//  Kooking
+//
+//  Created by Kayky Belleboni Casagrande on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct KookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
